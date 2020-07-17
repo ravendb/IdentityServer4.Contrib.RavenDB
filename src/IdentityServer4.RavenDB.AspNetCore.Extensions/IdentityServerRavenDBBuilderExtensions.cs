5971c817b0fe88bf3f7d73579a9d386d324fb7ca
Dejan Milicic <dejan.milicic@gmail.com>
2020-07-17 21:13:56 +0200

    asp.net middleware registration methods

---
+++ src/IdentityServer4.RavenDB.AspNetCore.Extensions/IdentityServerRavenDBBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer4.RavenDB.Storage.Stores;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer4.RavenDB.AspNetCore.Extensions
+{
+    public static class IdentityServerRavenDBBuilderExtensions
+    {
+        public static IIdentityServerBuilder AddConfigurationStore(
+            this IIdentityServerBuilder builder, IConfiguration configuration)
+        {
+            //builder.Services.Configure<RavenDBConfiguration>(configuration);
+
+            return builder.AddConfigurationStore();
+        }
+
+        private static IIdentityServerBuilder AddConfigurationStore(
+            this IIdentityServerBuilder builder)
+        {
+            builder.Services.AddTransient<IClientStore, ClientStore>();
+            builder.Services.AddTransient<IResourceStore, ResourceStore>();
+            //builder.Services.AddTransient<ICorsPolicyService, CorsPolicyService>();
+
+            return builder;
+        }
+    }
+}
