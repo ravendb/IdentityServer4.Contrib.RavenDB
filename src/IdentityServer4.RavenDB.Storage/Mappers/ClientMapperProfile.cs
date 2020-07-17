1511a63ad71d24bc25afe11a7c1cbc43a9f57fe7
Dejan Milicic <dejan.milicic@gmail.com>
2020-07-18 01:00:35 +0200

    mapping

--- src/IdentityServer4.RavenDB.Storage/Mappers/ClientMapperProfile.cs
+++ src/IdentityServer4.RavenDB.Storage/Mappers/ClientMapperProfile.cs
@@ -15,10 +15,10 @@ namespace IdentityServer4.RavenDB.Storage.Mappers
                 .ForMember(dest => dest.ProtocolType, opt => opt.Condition(srs => srs != null))
                 //.ForMember(x => x.AllowedIdentityTokenSigningAlgorithms, opts => opts.ConvertUsing(AllowedSigningAlgorithmsConverter.Converter, x => x.AllowedIdentityTokenSigningAlgorithms))
                 .ReverseMap();
-                //.ForMember(x => x.AllowedIdentityTokenSigningAlgorithms, opts => opts.ConvertUsing(AllowedSigningAlgorithmsConverter.Converter, x => x.AllowedIdentityTokenSigningAlgorithms));
+            //.ForMember(x => x.AllowedIdentityTokenSigningAlgorithms, opts => opts.ConvertUsing(AllowedSigningAlgorithmsConverter.Converter, x => x.AllowedIdentityTokenSigningAlgorithms));
 
-            CreateMap<Entities.ClientClaim, ClientClaim>(MemberList.None)
-                .ConstructUsing(src => new ClientClaim{Type = src.Type, Value = src.Value})
+            CreateMap<Entities.ClientClaim, Claim>(MemberList.None)
+                .ConstructUsing(src => new Claim(src.Type, src.Value))
                 .ReverseMap();
 
             CreateMap<Entities.Secret, Secret>(MemberList.Destination)
