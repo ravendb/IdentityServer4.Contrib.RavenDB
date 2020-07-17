5077568d012965ec7f6fd044a827800f2da75fc9
Dejan Milicic <dejan.milicic@gmail.com>
2020-07-18 00:27:19 +0200

    mapping fix

--- src/IdentityServer4.RavenDB.Storage/Mappers/ClientMapperProfile.cs
+++ src/IdentityServer4.RavenDB.Storage/Mappers/ClientMapperProfile.cs
@@ -17,9 +17,9 @@ namespace IdentityServer4.RavenDB.Storage.Mappers
                 .ReverseMap();
                 //.ForMember(x => x.AllowedIdentityTokenSigningAlgorithms, opts => opts.ConvertUsing(AllowedSigningAlgorithmsConverter.Converter, x => x.AllowedIdentityTokenSigningAlgorithms));
 
-            //CreateMap<Entities.ClientClaim, ClientClaim>(MemberList.None)
-            //    .ConstructUsing(src => new ClientClaim(src.Type, src.Value, ClaimValueTypes.String))
-            //    .ReverseMap();
+            CreateMap<Entities.ClientClaim, ClientClaim>(MemberList.None)
+                .ConstructUsing(src => new ClientClaim{Type = src.Type, Value = src.Value})
+                .ReverseMap();
 
             CreateMap<Entities.Secret, Secret>(MemberList.Destination)
                 .ForMember(dest => dest.Type, opt => opt.Condition(srs => srs != null))
