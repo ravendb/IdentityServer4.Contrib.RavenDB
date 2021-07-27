66bc1b71f569c20e759bfb32c544bba890fd6820
Lukasz Dobrzynski <lukasz@ravendb.net>
2021-07-27 10:50:20 +0200

    update RavenDB client to 4.2.115

--- src/IdentityServer4.RavenDB.Storage/Helpers/CryptographyHelper.cs
+++ src/IdentityServer4.RavenDB.Storage/Helpers/CryptographyHelper.cs
@@ -2,7 +2,7 @@
 
 namespace IdentityServer4.RavenDB.Storage.Helpers
 {
-    public static class CryptographyHelper
+    internal static class CryptographyHelper
     {
         public static string CreateHash(string value)
         {
